714020a2f009313c96af7c54de5840f059723f0b
Endermaiter <91609087+Endermaiter@users.noreply.github.com>
2021-11-16 10:48:18 +0100

    Add files via upload

---
+++ boletin 2.1/src/boletin/pkg2/pkg1/Boletin21.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg2.pkg1;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base=4,altura=3;
+        System.out.println("O área do triángulo é igual a "+ base*altura/2);
+    }
+    
+}
